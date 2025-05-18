04f678db762e0d2e21518bd4a8e5b4988448acb3
Simonas Radžius <47453208+Reflexxion19@users.noreply.github.com>
2025-05-18 14:28:29 +0300

    Minor fixes

--- sistema/crypto.php
+++ sistema/crypto.php
@@ -9,22 +9,6 @@ $server_base64_public_key = getPublicKey();
 $rawData = file_get_contents("php://input");
 
 if (!isset($_SERVER["CONTENT_TYPE"]) || $_SERVER["CONTENT_TYPE"] !== "application/json") {
-    if(isset($_GET['generate_key_pair'])) {
-        $keypair = generateRandomKeyPair();
-        $public_key = $keypair['public_key'];
-        $private_key = $keypair['private_key'];
-    
-        $base64_public_key = base64_encode($public_key);
-        $base64_private_key = base64_encode($private_key);
-        
-        echo '<div>';
-        echo '<h2>Generated Key Pair</h2>';
-        echo '<p>Public Key (Base64): '. htmlspecialchars($base64_public_key, ENT_QUOTES, 'UTF-8') . '</p>';
-        echo '<p>Private Key (Base64): '. htmlspecialchars($base64_private_key, ENT_QUOTES, 'UTF-8') . '</p>';
-        echo '</div>';
-        exit();
-    }
-
     header("HTTP/1.1 400 Bad Request");
     exit();
 } else {
